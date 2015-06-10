a3d66615376f3342704985913f29cef2b0bcc044
Drew Finstrom & Jessica Engel <student@epicodus.com>
2015-06-10 14:30:44 -0700

    fix issue where updating a form with values having the same name as properties on another model updated the wrong model

--- controllers/lot.js
+++ controllers/lot.js
@@ -1,5 +1,18 @@
 Auction.LotController = Ember.ObjectController.extend({
+  needs: ['lot'],
   actions: {
+    saveItem: function() {
+      var newItem = this.store.createRecord('item', {
+        title: this.get('itemTitle'),
+        description: this.get('itemDescription'),
+        imageURL: this.get('itemImageURL')
+      });
+      newItem.save();
+
+      var lot = this.get('controllers.lot.model');
+      lot.get('items').pushObject(newItem);
+      lot.save();
+    },
     delete: function() {
       if(confirm('Are you sure?')) {
         this.get('model').destroyRecord();
