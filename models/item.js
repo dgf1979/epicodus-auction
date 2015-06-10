cfc4feae596d4cc9aca1e61d62d03823edd415b6
Drew Finstrom & Jessica Engel <student@epicodus.com>
2015-06-10 13:49:52 -0700

    re-add model to item relationships after bug fix

--- models/item.js
+++ models/item.js
@@ -2,6 +2,5 @@ Auction.Item = DS.Model.extend({
   title: DS.attr(),
   description: DS.attr(),
   imageURL: DS.attr(),
-  // lot: DS.belongsTo('lot', {async: true})
-
+  lot: DS.belongsTo('lot', {async: true})
 });
